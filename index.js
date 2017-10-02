bd8d7daab77c13a121d675890d505895dd98c567
Lionel T <elrumordelaluz@hotmail.com>
2017-10-02 17:46:45 +0200

    Switch to Google API

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
+const fs = require('fs')
+const path = require('path')
 const TelegramBot = require('node-telegram-bot-api')
 const fetch = require('node-fetch')
-const NodeGeocoder = require('node-geocoder')
-const geolib = require('geolib')
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
   polling: true,
 })
 const geocoder = NodeGeocoder({ provider: 'google' })
 
+const tmp = require('tmp')
+
 bot.on('text', msg => {
   const points = []
   fetch('http://api.open-notify.org/iss-now.json')
     .then(res => res.json())
-    .then(({ iss_position: { latitude: lat, longitude: lng }, timestamp }) => {
-      points.push({ lat, lng, timestamp })
-      geocoder.reverse({ lat, lon: lng }, (err, res) => {
-        console.log(res, 0)
-      })
-      setTimeout(() => {
-        fetch('http://api.open-notify.org/iss-now.json')
-          .then(res => res.json())
-          .then(
-            ({ iss_position: { latitude: lat, longitude: lng }, timestamp }) => {
-              points.push({ lat, lng, timestamp })
-              geocoder.reverse({ lat, lon: lng }, (err, res) => {
-                console.log(res, 1)
-              })
-              console.log(
-                geolib.getSpeed(
-                  {
-                    lat: points[0].lat,
-                    lng: points[0].lat,
-                    time: points[0].timestamp,
-                  },
-                  {
-                    lat: points[1].lat,
-                    lng: points[1].lat,
-                    time: points[1].timestamp,
-                  },
-                  {unit: 'kmh'}
-                ), points
-              )
-            }
-          )
-      }, 1000)
+    .then(({ iss_position: { latitude, longitude }, timestamp }) => {
+      console.log({ latitude, longitude })
+      fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process
+          .env.GMAPS_API_KEY}`
+      )
+        .then(res => res.json())
+        .then(places => console.log(places))
+      fetch(
+        `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&size=600x300&zoom=5&maptype=hybrid&key=${process
+          .env.GMAPS_API_KEY}`
+      )
+        .then(res => {
+          return res.buffer()
+        })
+        .then(buffer => {
+          tmp.file((err, path, fd, cleanupCallback) => {
+            if (err) throw err
+
+            fs.writeFile(path, buffer, err => {
+              if (err) {
+                console.log(err)
+              }
+              bot.sendPhoto(msg.chat.id, path).then(() => cleanupCallback())
+            })
+          })
+        })
     })
 })
